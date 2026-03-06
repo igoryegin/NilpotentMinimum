d739c648474fb2bb50f2030d03366f852e32fddc
igy <miellago98@gmail.com>
2026-03-06 23:09:37 +0000

    added neg = "refdp"

--- R/nilmin.residuum.R
+++ R/nilmin.residuum.R
@@ -1,4 +1,4 @@
-nilmin.residuum <- function(x, y, neg = c("dombi", "rdp"), fixpoint = 0.5) {
+nilmin.residuum <- function(x, y, neg = c("dombi", "refdp"), fixpoint = 0.5) {
   neg <- match.arg(neg)
   if(!is.numeric(x) | !is.numeric(y))
     stop('x and/or y is not numeric')
@@ -9,11 +9,11 @@ nilmin.residuum <- function(x, y, neg = c("dombi", "rdp"), fixpoint = 0.5) {
            1,
            pmax(neg.dombi(x, fixpoint = fixpoint), y, na.rm = TRUE)
     )
-  else if(neg == "rdp")
+  else if(neg == "refdp")
     ifelse(x <= y,
            1,
-           pmax(neg.rdp(x, fixpoint = fixpoint), y, na.rm = TRUE)
+           pmax(neg.refdp(x, fixpoint = fixpoint), y, na.rm = TRUE)
     )
   else
-    stop('negation must be one of c("dombi", "rdp")')
+    stop('negation must be one of c("dombi", "refdp")')
 }
