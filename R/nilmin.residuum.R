9fdb2263229bb7e5c71bebb0327ae63ba14ee6a1
igy <miellago98@gmail.com>
2026-03-07 22:19:51 +0000

    fuzzy negation upgrade

--- R/nilmin.residuum.R
+++ R/nilmin.residuum.R
@@ -1,19 +1,19 @@
-nilmin.residuum <- function(x, y, neg = c("dombi", "refdp"), fixpoint = 0.5) {
+nilmin.residuum <- function(x, y, type = c("strong", "weak"), fixpoint = 0.5) {
   neg <- match.arg(neg)
   if(!is.numeric(x) | !is.numeric(y))
     stop('x and/or y is not numeric')
   if(any(c(x, y) < 0) | any(c(x, y) > 1))
     stop('All fuzzy truth values must be in the [0, 1] interval')
-  if(neg == "dombi")
+  if(type == "strong")
     ifelse(x <= y,
            1,
            pmax(neg.dombi(x, fixpoint = fixpoint), y, na.rm = TRUE)
     )
-  else if(neg == "refdp")
+  else if(type == "weak")
     ifelse(x <= y,
            1,
            pmax(neg.refdp(x, fixpoint = fixpoint), y, na.rm = TRUE)
     )
   else
-    stop('negation must be one of c("dombi", "refdp")')
+    stop('t-norm type must be one of c("strong", "weak")')
 }
