f4c38830abbce998ca2bc3a0c74ccad50ab59b74
igoryegin <miellago98@gmail.com>
2026-04-22 21:57:33 +0100

    type ordsum for nilmin.residuum

--- R/nilmin.residuum.R
+++ R/nilmin.residuum.R
@@ -1,6 +1,6 @@
 #' @export
 
-nilmin.residuum <- function(x, y, type = c("strong", "weak"), fixpoint = 0.5) {
+nilmin.residuum <- function(x, y, type = c("strong", "weak", "ordsum"), fixpoint = 0.5, alpha = 1/3) {
   type <- match.arg(type)
   if(!is.numeric(x) | !is.numeric(y))
     stop('x and/or y is not numeric')
@@ -16,6 +16,15 @@ nilmin.residuum <- function(x, y, type = c("strong", "weak"), fixpoint = 0.5) {
            1,
            pmax(neg.revdp(x, fixpoint = fixpoint), y, na.rm = TRUE)
     )
+  else if(type == "ordsum") {
+	if(alpha < 0 | alpha > 0.5)
+		stop('alpha must be in [0, 1/2]')
+	ifelse(x <= y,
+		   1,
+		   ifelse(x > y & alpha <= y & x <= 1 - alpha, 
+				  1 - x + y - alpha,
+				  pmax(1 - x, y))) 
+	}
   else
-    stop('t-norm type must be one of c("strong", "weak")')
+    stop('t-norm type must be one of c("strong", "weak", "ordsum")')
 }
